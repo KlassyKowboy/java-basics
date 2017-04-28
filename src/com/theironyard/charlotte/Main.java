f78f9f1dae141d25c20918831db0e44440f93550
William Rodgers <Blake@Williams-MacBook-Air.local>
2017-04-28 10:25:59 -0400

    basic stuffs zzz

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -3,6 +3,26 @@ package com.theironyard.charlotte;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // creating variables
+
+        String name = "Bob";
+        int age = 56;
+        boolean isAlive = true;
+
+        // creating an object using a
+        Person bob = new Person("bob", 56, true);
+
+        // if if/else statements
+
+        if (name != null) {
+            System.out.println("Hello, %s.", name);
+        } else {
+            System.out.println("You have NO name?");
+        }
+
+        do {
+            System.out.printf()
+        }
     }
 }
---
+++ src/com/theironyard/charlotte/Person.java
@@ -0,0 +1,14 @@
+package com.theironyard.charlotte;
+
+public class Person {
+
+    String name;
+    int age;
+    boolean isAlive;
+
+    public Person (String name, int age, boolean isAlive) {
+        this.name = name;
+        this.age = age;
+        this.isAlive = isAlive;
+    }
+}
